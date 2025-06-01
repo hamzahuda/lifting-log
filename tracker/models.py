262cec852248c8e8ff314aed7ce791ad34ff8082
hamzahuda <->
2025-06-01 13:24:40 +0100

    Create template models

--- tracker/models.py
+++ tracker/models.py
@@ -22,3 +22,22 @@ class Set(models.Model):
     reps = models.FloatField()
     weight = models.FloatField()
     notes = models.TextField(blank=True)
+
+
+class WorkoutTemplate(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    notes = models.TextField(blank=True)
+
+class ExerciseTemplate(models.Model):
+    workout_template = models.ForeignKey(WorkoutTemplate, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    rest_period = models.DurationField()
+    min_reps = models.IntegerField()
+    max_reps = models.IntegerField()
+    notes = models.TextField(blank=True)
+
+class SetTemplate(models.Model):
+    exercise_template = models.ForeignKey(ExerciseTemplate, on_delete=models.CASCADE)
+    notes = models.TextField(blank=True)
