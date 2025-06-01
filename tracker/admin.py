9b9bd450467852a0eaa8d7a1dc3a94b4fd997240
hamzahuda <->
2025-06-01 16:17:05 +0100

    Register template models to admin site

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Workout, Exercise, Set
+from .models import Workout, Exercise, Set, WorkoutTemplate, ExerciseTemplate, SetTemplate
 
 # Register your models here.
 admin.site.register(Workout)
 admin.site.register(Exercise)
 admin.site.register(Set)
+admin.site.register(WorkoutTemplate)
+admin.site.register(ExerciseTemplate)
+admin.site.register(SetTemplate)
