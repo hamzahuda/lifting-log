4d345483c797f4417805e2dba0462e261a9db0cd
hamzahuda <->
2025-06-01 12:52:29 +0100

    Let notes be blank

--- tracker/models.py
+++ tracker/models.py
@@ -6,7 +6,7 @@ class Workout(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     date = models.DateField()
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
 
 
 class Exercise(models.Model):
@@ -15,10 +15,10 @@ class Exercise(models.Model):
     rest_period = models.DurationField()
     min_reps = models.IntegerField()
     max_reps = models.IntegerField()
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
 
 class Set(models.Model):
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     reps = models.FloatField()
     weight = models.FloatField()
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
