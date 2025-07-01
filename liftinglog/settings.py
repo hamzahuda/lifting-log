6fb3947a78d5da477947ecd9745056652992dd70
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-07-01 13:05:46 +0100

    Change cors allowed origins

--- liftinglog/settings.py
+++ liftinglog/settings.py
@@ -137,6 +137,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
 ]
