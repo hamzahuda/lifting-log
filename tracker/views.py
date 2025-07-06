19bbb4bf6d69acc5bfe40782e6a7ad8bd4eb650b
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-07-06 15:42:25 +0100

    Workout create, list, delete views

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,6 @@
 from .models import *
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import *
 
 
@@ -8,3 +8,21 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class WorkoutListCreate(generics.ListCreateAPIView):
+    serializer_class = WorkoutSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Workout.objects.filter(user=user)
+
+
+class WorkoutDelete(generics.DestroyAPIView):
+    serializer_class = WorkoutSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Workout.objects.filter(author=user)
