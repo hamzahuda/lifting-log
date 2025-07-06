fee747ed169bfd41813a6af739400078466378fc
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-07-06 15:49:45 +0100

    Logout when going to register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import LoginRegisterForm from "../components/LoginRegisterForm.jsx";
 
 export default function Register() {
+    localStorage.clear();
     return <LoginRegisterForm route="/user/register/" method="register" />;
 }
