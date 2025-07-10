a01546cd267fa7055891b57c445a0d3d4a2973fa
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-07-10 14:58:07 +0100

    basic home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+
 export default function Home() {
+    const [workouts, setWorkouts] = useState([]);
+
+    useEffect(() => {
+        getWorkouts();
+    }, []);
+
+    function getWorkouts() {
+        api.get("/workouts/")
+            .then((result) => {
+                setWorkouts(result.data);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
     return (
         <div>
             <h1>Home Page</h1>
+            <h2>Your Workouts</h2>
+            <div>
+                {workouts.map((workout) => (
+                    <div key={workout.id}>
+                        <h3>{workout.name}</h3>
+                        <p>Date: {workout.date}</p>
+                        <p>Notes: {workout.notes}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
