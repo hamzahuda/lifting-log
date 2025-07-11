053168c6da881d2940b97d99c404c30c5c3d2acb
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-07-11 15:57:37 +0100

    Add temp navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,25 +5,29 @@ import Login from "./pages/Login.jsx";
 import Logout from "./components/Logout.jsx";
 import Register from "./pages/Register.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <ProtectedRoute>
-                            {/* This is where the main application code that requires authentication goes */}
-                            <Home />
-                        </ProtectedRoute>
-                    }
-                />
-                <Route path="/login" element={<Login />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/*" element={<NotFound />} />
-            </Routes>
+            <Navbar />
+            <div>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <ProtectedRoute>
+                                {/* This is where the main application code that requires authentication goes */}
+                                <Home />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/logout" element={<Logout />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/*" element={<NotFound />} />
+                </Routes>
+            </div>
         </BrowserRouter>
     );
 }
