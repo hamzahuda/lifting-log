1250bde73d5a3bcfc87476bdd30b34edcec7287b
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-08-25 13:02:20 +0100

    Add template editing

---
+++ frontend/src/components/EditTemplateForm.jsx
@@ -0,0 +1,194 @@
+import { useState } from "react";
+import api from "../api";
+
+function EditTemplateForm({ originalTemplate, onSave }) {
+    const [name, setName] = useState(originalTemplate.name);
+    const [notes, setNotes] = useState(originalTemplate.notes);
+    const [exercises, setExercises] = useState(
+        originalTemplate.exercise_templates
+    );
+
+    // Exercise handlers
+    const handleAddExercise = () => {
+        setExercises([
+            ...exercises,
+            {
+                name: "",
+                rest_period: "00:02:00",
+                min_reps: 8,
+                max_reps: 12,
+                notes: "",
+                set_templates: [{ notes: "" }, { notes: "" }, { notes: "" }],
+            },
+        ]);
+    };
+
+    const handleExerciseChange = (index, event) => {
+        const newExercises = [...exercises];
+        newExercises[index][event.target.name] = event.target.value;
+        setExercises(newExercises);
+    };
+
+    const handleRemoveExercise = (index) => {
+        const newExercises = [...exercises];
+        newExercises.splice(index, 1);
+        setExercises(newExercises);
+    };
+
+    // Set handlers
+    const handleAddSet = (exerciseIndex) => {
+        const newExercises = [...exercises];
+        newExercises[exerciseIndex].set_templates.push({ notes: "" });
+        setExercises(newExercises);
+    };
+
+    const handleSetChange = (exerciseIndex, setIndex, event) => {
+        const newExercises = [...exercises];
+        newExercises[exerciseIndex].set_templates[setIndex][event.target.name] =
+            event.target.value;
+        setExercises(newExercises);
+    };
+
+    const handleRemoveSet = (exerciseIndex, setIndex) => {
+        const newExercises = [...exercises];
+        newExercises[exerciseIndex].set_templates.splice(setIndex, 1);
+        setExercises(newExercises);
+    };
+
+    // Form submission
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const templateData = {
+            name,
+            notes,
+            exercise_templates: exercises,
+        };
+
+        try {
+            await api.put(
+                `/workout-templates/${originalTemplate.id}/`,
+                templateData
+            );
+            alert("Template edited successfully!");
+
+            // Refresh template list
+            onSave();
+        } catch (error) {
+            alert("Failed to edit template: " + error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Create New Template</h2>
+            <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Template Name (e.g., Upper Body)"
+                required
+            />
+            <textarea
+                name="notes"
+                value={notes}
+                onChange={(e) => setNotes(e.target.value)}
+                placeholder="Template Notes (e.g., Focus on strength)"
+            />
+
+            <h3>Exercises</h3>
+            {exercises.map((exercise, exerciseIndex) => (
+                <div key={exerciseIndex}>
+                    <h4>Exercise {exerciseIndex + 1}</h4>
+                    <input
+                        type="text"
+                        name="name"
+                        value={exercise.name}
+                        onChange={(e) => handleExerciseChange(exerciseIndex, e)}
+                        placeholder="Exercise Name (e.g., Bench Press)"
+                        required
+                    />
+                    <input
+                        type="text"
+                        name="rest_period"
+                        value={exercise.rest_period}
+                        onChange={(e) => handleExerciseChange(exerciseIndex, e)}
+                        placeholder="Rest Period (HH:MM:SS)"
+                        required
+                    />
+                    <div>
+                        <input
+                            type="number"
+                            name="min_reps"
+                            value={exercise.min_reps}
+                            onChange={(e) =>
+                                handleExerciseChange(exerciseIndex, e)
+                            }
+                            placeholder="Min Reps"
+                            required
+                        />
+                        <input
+                            type="number"
+                            name="max_reps"
+                            value={exercise.max_reps}
+                            onChange={(e) =>
+                                handleExerciseChange(exerciseIndex, e)
+                            }
+                            placeholder="Max Reps"
+                            required
+                        />
+                    </div>
+                    <textarea
+                        name="notes"
+                        value={exercise.notes}
+                        onChange={(e) => handleExerciseChange(exerciseIndex, e)}
+                        placeholder="Exercise Notes"
+                    />
+
+                    {exercise.set_templates.map((set, setIndex) => (
+                        <div key={setIndex}>
+                            <label>Set {setIndex + 1}</label>
+                            <input
+                                type="text"
+                                name="notes"
+                                value={set.notes}
+                                onChange={(e) =>
+                                    handleSetChange(exerciseIndex, setIndex, e)
+                                }
+                                placeholder="Set Notes (e.g., Warm-up set)"
+                            />
+                            <button
+                                type="button"
+                                onClick={() =>
+                                    handleRemoveSet(exerciseIndex, setIndex)
+                                }
+                            >
+                                Remove Set
+                            </button>
+                        </div>
+                    ))}
+                    <button
+                        type="button"
+                        onClick={() => handleAddSet(exerciseIndex)}
+                    >
+                        Add Set
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() => handleRemoveExercise(exerciseIndex)}
+                    >
+                        Remove Exercise
+                    </button>
+                </div>
+            ))}
+            <button type="button" onClick={handleAddExercise}>
+                Add Exercise
+            </button>
+            <br />
+            <button type="submit">Save Edits</button>
+        </form>
+    );
+}
+
+export default EditTemplateForm;
--- frontend/src/pages/TemplateDetail.jsx
+++ frontend/src/pages/TemplateDetail.jsx
@@ -1,53 +1,87 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router";
 import api from "../api";
+import EditTemplateForm from "../components/EditTemplateForm";
 
 function TemplateDetail() {
     const { id } = useParams();
     const [template, setTemplate] = useState(null);
+
+    const [isEditing, setIsEditing] = useState(false);
+
     let navigate = useNavigate();
 
-    useEffect(() => {
+    const getTemplate = () => {
         api.get(`/workout-templates/${id}/`)
             .then((res) => {
                 setTemplate(res.data);
             })
             .catch((err) => alert(err));
-    }, []);
+    };
+
+    useEffect(() => {
+        getTemplate();
+    }, [id]);
 
     if (!template) {
         return <div>Loading template...</div>;
     }
 
+    const handleSave = () => {
+        setIsEditing(false);
+        getTemplate();
+    };
+
     const handleDeleteTemplate = () => {
-        api.delete(`/workout-templates/${id}/`)
-            .then(() => {
-                navigate("/templates/");
-            })
-            .catch((err) => alert(err));
+        if (window.confirm("Are you sure you want to delete this template?")) {
+            api.delete(`/workout-templates/${id}/`)
+                .then(() => {
+                    alert("Template deleted successfully!");
+                    navigate("/templates");
+                })
+                .catch((err) => alert(err));
+        }
     };
 
     return (
         <div>
-            <h1>Template Name: {template.name}</h1>
-            <p>Template Notes: {template.notes}</p>
-            {template.exercise_templates.map((exercise) => (
-                <div key={exercise.id}>
-                    <p>--</p>
-                    <p>{exercise.name}</p>
-                    <p>
-                        {exercise.min_reps} - {exercise.max_reps} reps
-                    </p>
-                    <p>{exercise.rest_period} rest</p>
-                    <p>Notes: {exercise.notes}</p>
+            {isEditing ? (
+                <EditTemplateForm
+                    originalTemplate={template}
+                    onSave={handleSave}
+                />
+            ) : (
+                <>
+                    <h1>Template Name: {template.name}</h1>
+                    <p>Template Notes: {template.notes}</p>
+                    {template.exercise_templates.map((exercise) => (
+                        <div key={exercise.id}>
+                            <p>--</p>
+                            <p>{exercise.name}</p>
+                            <p>
+                                {exercise.min_reps} - {exercise.max_reps} reps
+                            </p>
+                            <p>{exercise.rest_period} rest</p>
+                            <p>Notes: {exercise.notes}</p>
 
-                    {exercise.set_templates.map((set, index) => (
-                        <p key={set.id}>
-                            Set {index + 1} {set.notes ? `(${set.notes})` : ""}
-                        </p>
+                            {exercise.set_templates.map((set, index) => (
+                                <p key={set.id}>
+                                    Set {index + 1}{" "}
+                                    {set.notes ? `(${set.notes})` : ""}
+                                </p>
+                            ))}
+                        </div>
                     ))}
-                </div>
-            ))}
+                </>
+            )}
+            <button
+                type="button"
+                onClick={() => {
+                    setIsEditing(!isEditing);
+                }}
+            >
+                {isEditing ? "Exit Editing Without Saving" : "Edit Template"}
+            </button>
             <button type="button" onClick={handleDeleteTemplate}>
                 Delete Template
             </button>
