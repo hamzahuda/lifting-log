884bfdf31459d978f408aaa1cd225075aeb0ff09
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-08-25 15:35:43 +0100

    remove logout when going to register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import LoginRegisterForm from "../components/LoginRegisterForm.jsx";
 
 export default function Register() {
-    localStorage.clear();
     return <LoginRegisterForm route="/users/" method="register" />;
 }
