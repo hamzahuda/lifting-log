c5842828ebae2f7394776a8278b01bc99edbcaef
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-08-27 13:27:41 +0100

    remove workout create and update logic from views

--- tracker/views.py
+++ tracker/views.py
@@ -40,38 +40,3 @@ class WorkoutViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         return Workout.objects.filter(user=user)
-
-    def create(self, request, *args, **kwargs):
-        template_id = request.data.get("template")
-        if not template_id:
-            return Response(
-                {"error": "A 'template' ID is required to create a workout."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        try:
-            template = WorkoutTemplate.objects.get(pk=template_id, user=request.user)
-        except WorkoutTemplate.DoesNotExist:
-            return Response(
-                {"error": "Workout template not found."},
-                status=status.HTTP_404_NOT_FOUND,
-            )
-
-        date = request.data.get("date", datetime.date.today())
-        new_workout = Workout.create_from_template(
-            user=request.user, template=template, date=date
-        )
-
-        serializer = self.get_serializer(new_workout)
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
-
-    def update(self, request, *args, **kwargs):
-        workout_instance = self.get_object()
-
-        updated_workout = workout_instance.update_with_exercises(request.data)
-
-        serializer = self.get_serializer(updated_workout)
-        return Response(serializer.data)
