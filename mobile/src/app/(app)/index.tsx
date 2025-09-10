ba94c1dde035d6604877d918b1977ce7d4ce7d43
hamzahuda <>
2025-09-10 11:59:46 +0100

    Move logic out of index page

--- mobile/src/app/(app)/index.tsx
+++ mobile/src/app/(app)/index.tsx
@@ -1,30 +1,15 @@
-import { useState, useEffect } from "react";
-import { supabase } from "../../utils/supabase";
-import Auth from "../../components/Auth";
-import Account from "../../components/Account";
-import { View } from "react-native";
-import { Session } from "@supabase/supabase-js";
+import { Text, View } from "react-native";
 
-export default function App() {
-    const [session, setSession] = useState<Session | null>(null);
-
-    useEffect(() => {
-        supabase.auth.getSession().then(({ data: { session } }) => {
-            setSession(session);
-        });
-
-        supabase.auth.onAuthStateChange((_event, session) => {
-            setSession(session);
-        });
-    }, []);
-
+export default function Workout() {
     return (
-        <View>
-            {session && session.user ? (
-                <Account key={session.user.id} session={session} />
-            ) : (
-                <Auth />
-            )}
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Index Page</Text>
         </View>
     );
 }
