3c09fdbd8608359440693d0f100d6c9a0a656b97
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-09-27 17:24:32 +0100

    Remove unused import

--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .permissions import *
 from .serializers import *
-import datetime
 
 
 class UserViewSet(viewsets.ModelViewSet):
