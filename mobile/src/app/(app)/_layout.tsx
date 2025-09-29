ce2f02b4750853c71759a3f80796b1ead50dc77f
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-09-29 19:41:34 +0100

    Add tabs to base layout with basic customisation

--- mobile/src/app/(app)/_layout.tsx
+++ mobile/src/app/(app)/_layout.tsx
@@ -1,5 +1,29 @@
 import { Tabs } from "expo-router";
 
 export default function AppLayout() {
-    return <Tabs />;
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="workouts"
+                options={{
+                    title: "Home",
+                    headerShown: false,
+                }}
+            />
+            <Tabs.Screen
+                name="templates"
+                options={{
+                    title: "Templates",
+                    headerShown: false,
+                }}
+            />
+            <Tabs.Screen
+                name="account"
+                options={{
+                    title: "Account",
+                    headerShown: false,
+                }}
+            />
+        </Tabs>
+    );
 }
