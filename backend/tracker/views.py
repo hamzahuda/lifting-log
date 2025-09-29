75fdda3e949338b266eab243a004aca48a407fc2
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-09-29 16:13:19 +0100

    Remove unused imports from views.py

--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,6 +1,5 @@
 from .models import *
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .permissions import *
 from .serializers import *
