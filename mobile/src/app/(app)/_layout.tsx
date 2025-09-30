553341272346363b935c08c6c98301629ea804ef
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-09-30 16:05:39 +0100

    Move headerShown option to parent

--- mobile/src/app/(app)/_layout.tsx
+++ mobile/src/app/(app)/_layout.tsx
@@ -2,26 +2,23 @@ import { Tabs } from "expo-router";
 
 export default function AppLayout() {
     return (
-        <Tabs>
+        <Tabs screenOptions={{ headerShown: false }}>
             <Tabs.Screen
                 name="workouts"
                 options={{
                     title: "Home",
-                    headerShown: false,
                 }}
             />
             <Tabs.Screen
                 name="templates"
                 options={{
                     title: "Templates",
-                    headerShown: false,
                 }}
             />
             <Tabs.Screen
                 name="account"
                 options={{
                     title: "Account",
-                    headerShown: false,
                 }}
             />
         </Tabs>
