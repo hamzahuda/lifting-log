fdfd69c66f6cf0e5fc19e14173e0a59fd5f70d78
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-09-30 15:08:00 +0100

    Add Template screem

--- mobile/src/app/(app)/templates/index.tsx
+++ mobile/src/app/(app)/templates/index.tsx
@@ -1,15 +1,76 @@
-import { Text, View } from "react-native";
+import React, { useState, useCallback } from "react";
+import {
+    Alert,
+    View,
+    Text,
+    TouchableOpacity,
+    ActivityIndicator,
+} from "react-native";
+import api from "@/utils/api";
+import { useRouter } from "expo-router";
+import { useFocusEffect } from "@react-navigation/native";
+import { WorkoutTemplate } from "@/types";
 
-export default function Index() {
+export default function TemplateScreen() {
+    const [templates, setTemplates] = useState<WorkoutTemplate[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const router = useRouter();
+
+    const getTemplates = useCallback(() => {
+        setLoading(true);
+        api.get<WorkoutTemplate[]>("/workout-templates/")
+            .then((result) => {
+                setTemplates(result.data);
+            })
+            .catch((err: Error) => {
+                console.error(err);
+                Alert.alert("Error", "Failed to fetch templates.");
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
+    useFocusEffect(
+        useCallback(() => {
+            getTemplates();
+        }, [getTemplates])
+    );
+
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Templates</Text>
+        <View className="flex-1 bg-background">
+            <View className="flex-1 p-5">
+                {loading ? (
+                    <View className="flex-1 justify-center items-center">
+                        <ActivityIndicator size="large" color="#3B82F6" />
+                    </View>
+                ) : (
+                    templates.map((template) => (
+                        <TouchableOpacity
+                            key={template.id}
+                            className="bg-primary rounded-2xl p-4 mb-4 shadow-md shadow-black"
+                            onPress={() =>
+                                router.push(`/templates/${template.id}`)
+                            }
+                        >
+                            <Text className="text-white font-bold text-xl">
+                                {template.name}
+                            </Text>
+                            {template.notes && (
+                                <Text className="text-gray-400 mt-1">
+                                    Notes: {template.notes}
+                                </Text>
+                            )}
+                        </TouchableOpacity>
+                    ))
+                )}
+            </View>
+            <TouchableOpacity
+                className="absolute bottom-4 right-4 w-16 h-16 bg-blue-500 rounded-2xl justify-center items-center shadow-lg"
+                onPress={() => router.push("/(app)/templates/create")}
+            >
+                <Text className="text-white text-3xl">+</Text>
+            </TouchableOpacity>
         </View>
     );
 }
