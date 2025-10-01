5d72d2b9e34796c66e94d483141b061fdd4c1f92
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-10-01 12:55:26 +0100

    Move header options to layout file

--- mobile/src/app/(app)/workouts/_layout.tsx
+++ mobile/src/app/(app)/workouts/_layout.tsx
@@ -18,6 +18,13 @@ export default function WorkoutsLayout() {
                     fontSize: 26,
                 },
             }}
-        />
+        >
+            <Stack.Screen name="index" options={{ headerTitle: "Workouts" }} />
+            <Stack.Screen
+                name="create"
+                options={{ headerTitle: "Create Workout" }}
+            />
+            <Stack.Screen name="[id]" options={{ headerShown: false }} />
+        </Stack>
     );
 }
