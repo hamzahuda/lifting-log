d881432c583cdea6efa97b32918eeb88ee6cc6fc
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-10-07 17:57:53 +0100

    Move colors to new colors.ts file

---
+++ mobile/src/styles/colors.ts
@@ -0,0 +1,9 @@
+module.exports = {
+    background: "rgb(247, 247, 247)",
+    primary: "#ffffff",
+    secondary: "#e9e9e9",
+    accent: "#437ace",
+    "t-primary": "#000000",
+    "t-secondary": "#5a5a5a",
+    "t-tertiary": "#5a5a5a",
+};
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("./src/styles/colors");
+
 module.exports = {
     content: [
         "./src/app/**/*.{js,ts,jsx,tsx}",
@@ -6,17 +9,7 @@ module.exports = {
     ],
     presets: [require("nativewind/preset")],
     theme: {
-        extend: {
-            colors: {
-                background: "rgb(247, 247, 247)",
-                primary: "#ffffff",
-                secondary: "#e9e9e9",
-                accent: "#437ace",
-                "t-primary": "#000000",
-                "t-secondary": "#5a5a5a",
-                "t-tertiary": "#5a5a5a",
-            },
-        },
+        extend: { colors },
     },
     plugins: [],
 };
