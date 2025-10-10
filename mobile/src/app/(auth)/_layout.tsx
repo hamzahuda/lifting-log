ad0e9670cbd70d0f9de7dc8742d92a3e389949e8
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-10-10 17:37:11 +0100

    Adjust auth screen animations

--- mobile/src/app/(auth)/_layout.tsx
+++ mobile/src/app/(auth)/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
-    return <Stack screenOptions={{ headerShown: false }} />;
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" options={{ animation: "none" }} />
+            <Stack.Screen name="sign-up" options={{ animation: "none" }} />
+            <Stack.Screen
+                name="forgot-password"
+                options={{ animation: "slide_from_bottom" }}
+            />
+        </Stack>
+    );
 }
