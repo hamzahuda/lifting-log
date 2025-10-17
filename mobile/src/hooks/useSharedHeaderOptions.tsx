0ad3ecd41921e82b3d73fa9b16071b537c68a14b
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-10-17 18:26:14 +0100

    Add useSharedHeaderOptions hook to use alongside header title component

---
+++ mobile/src/hooks/useSharedHeaderOptions.tsx
@@ -0,0 +1,20 @@
+import { useColorScheme } from "nativewind";
+import { THEME } from "@/utils/theme";
+import { ExtendedStackNavigationOptions } from "expo-router/build/layouts/StackClient";
+
+export default function useSharedHeaderOptions(): ExtendedStackNavigationOptions {
+    const { colorScheme } = useColorScheme();
+    const themeColors =
+        colorScheme === undefined ? THEME.dark : THEME[colorScheme];
+
+    return {
+        headerStyle: {
+            backgroundColor: themeColors.background,
+        },
+        headerTintColor: themeColors.foreground,
+        headerTitleStyle: {
+            fontSize: 26,
+        },
+        headerTitleAlign: "center" as const,
+    };
+}
