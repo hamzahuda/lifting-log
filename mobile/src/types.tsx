4426dd4cd931f1be6336358e4da1be0033f5897c
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-10-29 11:06:32 +0000

    Use types instead of interfaces

--- mobile/src/types.tsx
+++ mobile/src/types.tsx
@@ -1,49 +1,49 @@
-export interface Set {
+export type Set = {
     readonly id: number;
     reps: number | null;
     weight: number | null;
     min_reps: number;
     max_reps: number;
     notes: string;
-}
+};
 
-export interface Exercise {
+export type Exercise = {
     readonly id: number;
     name: string;
     rest_period: string;
     notes: string;
     sets: Set[];
-}
+};
 
-export interface Workout {
+export type Workout = {
     readonly id: number;
     name: string;
     date: string;
     notes: string;
     exercises: Exercise[];
-}
+};
 
-export interface SetTemplate {
+export type SetTemplate = {
     readonly id: number;
     notes: string;
     min_reps: number;
     max_reps: number;
-}
+};
 
-export interface ExerciseTemplate {
+export type ExerciseTemplate = {
     readonly id: number;
     name: string;
     rest_period: string;
     notes: string;
     set_templates: SetTemplate[];
-}
+};
 
-export interface WorkoutTemplate {
+export type WorkoutTemplate = {
     readonly id: number;
     readonly url: string;
     name: string;
     notes: string;
     exercise_templates: ExerciseTemplate[];
-}
+};
 
 export type WorkoutTemplateFormData = Omit<WorkoutTemplate, "id" | "url">;
