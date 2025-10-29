2675a52d86fa59ce0a0f8312fc3924a7dd6a603e
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-10-29 15:44:12 +0000

    Set up api for custom exercise name

--- backend/tracker/urls.py
+++ backend/tracker/urls.py
@@ -8,6 +8,11 @@ router.register(r"workouts", views.WorkoutViewSet, basename="workout")
 router.register(
     r"workout-templates", views.WorkoutTemplateViewSet, basename="workouttemplate"
 )
+router.register(
+    r"custom-exercise-names",
+    views.CustomExerciseNameViewSet,
+    basename="customexercisename",
+)
 
 urlpatterns = [
     path("", include(router.urls)),
