83e7347a5433100627c47816308080c6aa9cfae6
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2025-11-14 21:37:22 +0000

    Refactor ScreenStateWrapper to improve message handling and make props optional

--- mobile/src/components/common/screen-state-wrapper.tsx
+++ mobile/src/components/common/screen-state-wrapper.tsx
@@ -2,38 +2,50 @@ import { ActivityIndicator, Text, View } from "react-native";
 
 type ScreenStateWrapperProps = {
     isLoading: boolean;
-    isEmpty: boolean;
-    emptyMessage: string[];
     children: React.ReactNode;
+
+    // For list screens
+    isEmpty?: boolean;
+    emptyMessage?: string[];
+    // For detail screens
+    isNotFound?: boolean;
+    notFoundMessage?: string[];
+};
+
+const MessageDisplay = ({ message }: { message: string[] }) => {
+    return (
+        <View className="flex-1 justify-center items-center mb-60">
+            {message.map((text, index) => (
+                <Text key={index} className="text-muted-foreground text-center">
+                    {text}
+                </Text>
+            ))}
+        </View>
+    );
 };
 
 export default function ScreenStateWrapper({
     isLoading,
     isEmpty,
-    emptyMessage,
+    emptyMessage = ["No results."],
+    isNotFound,
+    notFoundMessage = ["Not found."],
     children,
 }: ScreenStateWrapperProps) {
     if (isLoading) {
         return (
-            <View className="flex-1 justify-center items-center">
+            <View className="flex-1 justify-center items-center mb-60">
                 <ActivityIndicator size="large" color="#3B82F6" />
             </View>
         );
     }
 
+    if (isNotFound) {
+        return <MessageDisplay message={notFoundMessage} />;
+    }
+
     if (isEmpty) {
-        return (
-            <View className="flex-1 flex-col justify-center mb-60">
-                {emptyMessage.map((message, index) => (
-                    <Text
-                        key={index}
-                        className="text-muted-foreground text-center"
-                    >
-                        {message}
-                    </Text>
-                ))}
-            </View>
-        );
+        return <MessageDisplay message={emptyMessage} />;
     }
 
     return <>{children}</>;
