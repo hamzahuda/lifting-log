7abebefe58de8519bf6fd94dc6f9ab968cfa9202
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2026-01-24 20:37:22 +0000

    Initial ProgressListScreen

--- mobile/src/app/(app)/progress/index.tsx
+++ mobile/src/app/(app)/progress/index.tsx
@@ -1,5 +1,58 @@
-import { View } from "react-native";
+import { useState, useCallback } from "react";
+import { ScrollView, View, Text } from "react-native";
+import { fetchPerformedExercises } from "@/services/api";
+import { useFocusEffect } from "@react-navigation/native";
+import ScreenStateWrapper from "@/components/common/screen-state-wrapper";
 
-export default function ProgressScreen() {
-    return <View></View>;
+export default function ProgressListScreen() {
+    const [exercises, setExercises] = useState<String[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const getWorkouts = useCallback(async () => {
+        try {
+            setLoading(true);
+            const res = await fetchPerformedExercises();
+            setExercises(res.data);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    useFocusEffect(
+        useCallback(() => {
+            getWorkouts();
+        }, [getWorkouts]),
+    );
+
+    return (
+        <View className="flex-1 bg-background p-2">
+            <ScreenStateWrapper
+                isLoading={loading}
+                isEmpty={exercises.length === 0}
+                emptyMessage={[
+                    "No exercises found,",
+                    "Complete your first workout to get started.",
+                ]}
+            >
+                <ScrollView
+                    contentContainerStyle={{ paddingBottom: 210 }}
+                    showsVerticalScrollIndicator={false}
+                >
+                    <View>
+                        {exercises.map((exercise, index) => (
+                            <View key={index}>
+                                <View className="flex-row items-center justify-between">
+                                    <Text className="text-foreground">
+                                        {exercise}
+                                    </Text>
+                                </View>
+                            </View>
+                        ))}
+                    </View>
+                </ScrollView>
+            </ScreenStateWrapper>
+        </View>
+    );
 }
