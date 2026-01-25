d9dd2e12fc61633c5903693a1a3013a7f55d38d5
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2026-01-25 14:35:55 +0000

    Create ProgressListItem component and use it in ProgressListScreen

--- mobile/src/app/(app)/progress/index.tsx
+++ mobile/src/app/(app)/progress/index.tsx
@@ -1,12 +1,16 @@
 import { useState, useCallback } from "react";
-import { ScrollView, View, Text } from "react-native";
+import { ScrollView, View } from "react-native";
 import { fetchPerformedExercises } from "@/services/api";
 import { useFocusEffect } from "@react-navigation/native";
 import ScreenStateWrapper from "@/components/common/screen-state-wrapper";
+import ProgressListItem from "./_components/ProgressListItem";
+import { useRouter } from "expo-router";
 
 export default function ProgressListScreen() {
-    const [exercises, setExercises] = useState<String[]>([]);
+    const [exercises, setExercises] = useState<string[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
+
+    const router = useRouter();
 
     const getWorkouts = useCallback(async () => {
         try {
@@ -42,13 +46,11 @@ export default function ProgressListScreen() {
                 >
                     <View>
                         {exercises.map((exercise, index) => (
-                            <View key={index}>
-                                <View className="flex-row items-center justify-between">
-                                    <Text className="text-foreground">
-                                        {exercise}
-                                    </Text>
-                                </View>
-                            </View>
+                            <ProgressListItem
+                                key={index}
+                                exercise={exercise}
+                                onPress={(exercise) => router.push(`/progress`)}
+                            />
                         ))}
                     </View>
                 </ScrollView>
