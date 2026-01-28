d37081edc13219ae0a4929eef81110c1b8f59c02
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2026-01-28 11:10:38 +0000

    rename to exercise_names and remove unnecessary setLoading

--- mobile/src/app/(app)/progress/index.tsx
+++ mobile/src/app/(app)/progress/index.tsx
@@ -7,14 +7,13 @@ import ProgressListItem from "./_components/ProgressListItem";
 import { useRouter } from "expo-router";
 
 export default function ProgressListScreen() {
-    const [exercises, setExercises] = useState<string[]>([]);
+    const [exercise_names, setExercises] = useState<string[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
     const router = useRouter();
 
     const getWorkouts = useCallback(async () => {
         try {
-            setLoading(true);
             const res = await fetchPerformedExercises();
             setExercises(res.data);
         } catch (error) {
@@ -34,9 +33,9 @@ export default function ProgressListScreen() {
         <View className="flex-1 bg-background p-2">
             <ScreenStateWrapper
                 isLoading={loading}
-                isEmpty={exercises.length === 0}
+                isEmpty={exercise_names.length === 0}
                 emptyMessage={[
-                    "No exercises found,",
+                    "No exercise_names found,",
                     "Complete your first workout to get started.",
                 ]}
             >
@@ -45,10 +44,10 @@ export default function ProgressListScreen() {
                     showsVerticalScrollIndicator={false}
                 >
                     <View>
-                        {exercises.map((exercise, index) => (
+                        {exercise_names.map((exercise_name, index) => (
                             <ProgressListItem
                                 key={index}
-                                exercise={exercise}
+                                exercise_name={exercise_name}
                                 onPress={(exercise) =>
                                     router.push(`/progress/${exercise}`)
                                 }
