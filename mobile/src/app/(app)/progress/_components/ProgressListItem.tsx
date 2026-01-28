0663d74154121a8310965fd7a879e21d41727af5
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2026-01-28 11:09:29 +0000

    Rename exercise to exercise_name to make it clearer in ProgressListItem

--- mobile/src/app/(app)/progress/_components/ProgressListItem.tsx
+++ mobile/src/app/(app)/progress/_components/ProgressListItem.tsx
@@ -1,22 +1,22 @@
 import { View, Text, TouchableOpacity } from "react-native";
 
 type TemplateListItemProps = {
-    exercise: string;
+    exercise_name: string;
     onPress: (name: string) => void;
 };
 
 export default function ProgressListItem({
-    exercise,
+    exercise_name,
     onPress,
 }: TemplateListItemProps) {
     return (
         <TouchableOpacity
             className="bg-background rounded-2xl p-4 mb-4 flex-row justify-between shadow-md border border-border"
-            onPress={() => onPress(exercise)}
+            onPress={() => onPress(exercise_name)}
         >
             <View className="flex-1">
                 <Text className="text-foreground font-bold text-xl">
-                    {exercise}
+                    {exercise_name}
                 </Text>
             </View>
         </TouchableOpacity>
