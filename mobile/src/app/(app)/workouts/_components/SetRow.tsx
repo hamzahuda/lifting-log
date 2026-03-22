2e57d3043fe5287f780900853551bc6de925d412
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2026-03-22 18:47:48 +0000

    Add delete button to SetRow and improve layout

--- mobile/src/app/(app)/workouts/_components/SetRow.tsx
+++ mobile/src/app/(app)/workouts/_components/SetRow.tsx
@@ -1,6 +1,8 @@
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator, TouchableOpacity } from "react-native";
 import { Set } from "@/types";
 import SetInput from "./SetInput";
+import { Trash2 } from "lucide-react-native";
+import { Icon } from "@/components/ui/icon";
 
 interface SetRowProps {
     set: Set;
@@ -14,6 +16,8 @@ interface SetRowProps {
         field: "weight" | "reps",
         value: string,
     ) => void;
+    isEditing: boolean;
+    onDeleteSet: (exerciseIndex: number, setIndex: number) => void;
 }
 
 export default function SetRow({
@@ -23,6 +27,8 @@ export default function SetRow({
     lastPerformanceSet,
     isLoadingLastPerformance,
     onSetUpdate,
+    isEditing,
+    onDeleteSet,
 }: SetRowProps) {
     const handleWeightChange = (value: string) => {
         onSetUpdate(exerciseIndex, setIndex, "weight", value);
@@ -34,30 +40,48 @@ export default function SetRow({
 
     return (
         <View className="flex-row py-1 mb-2 items-center">
-            <Text className="w-12 text-foreground text-lg text-center">
-                {setIndex + 1}
-            </Text>
-            <Text className="flex-1 text-muted-foreground text-lg text-center">
-                {isLoadingLastPerformance ? (
-                    <ActivityIndicator size="small" />
-                ) : (
-                    lastPerformanceSet
-                )}
-            </Text>
-            <View className="w-24 items-center">
-                <SetInput
-                    value={set.weight?.toString() ?? ""}
-                    onChangeText={handleWeightChange}
-                    placeholder="--"
-                />
+            <View className="flex-1 flex-row items-center">
+                <Text className="w-12 text-foreground text-lg text-center">
+                    {setIndex + 1}
+                </Text>
+                <View className="flex-1 justify-center items-center">
+                    {isLoadingLastPerformance ? (
+                        <ActivityIndicator size="small" />
+                    ) : (
+                        <Text
+                            className="text-muted-foreground text-lg text-center"
+                            numberOfLines={1}
+                            adjustsFontSizeToFit
+                        >
+                            {lastPerformanceSet}
+                        </Text>
+                    )}
+                </View>
+                <View className="w-24 items-center">
+                    <SetInput
+                        value={set.weight?.toString() ?? ""}
+                        onChangeText={handleWeightChange}
+                        placeholder="--"
+                    />
+                </View>
+                <View className="w-24 items-center">
+                    <SetInput
+                        value={set.reps?.toString() ?? ""}
+                        onChangeText={handleRepsChange}
+                        placeholder={`${set.min_reps}-${set.max_reps}`}
+                    />
+                </View>
             </View>
-            <View className="w-24 items-center">
-                <SetInput
-                    value={set.reps?.toString() ?? ""}
-                    onChangeText={handleRepsChange}
-                    placeholder={`${set.min_reps}-${set.max_reps}`}
-                />
-            </View>
+
+            {isEditing && (
+                <View className="w-6 items-center justify-center">
+                    <TouchableOpacity
+                        onPress={() => onDeleteSet(exerciseIndex, setIndex)}
+                    >
+                        <Icon as={Trash2} size={20} className="text-red-500" />
+                    </TouchableOpacity>
+                </View>
+            )}
         </View>
     );
 }
