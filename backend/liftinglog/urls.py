a6c7aa9b252d13b6ec44599b924c157595e43041
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2026-06-14 11:22:16 +0100

    Expose prometheus metrics

--- backend/liftinglog/urls.py
+++ backend/liftinglog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("tracker.urls")),
+    path("", include("django_prometheus.urls")),
 ]
